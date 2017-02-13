4d15a6d419307ee78d6fd222f851cd190086123e
brianlaw033 <brian9328hk@gmail.com>
2017-02-13 12:11:39 +0800

    concat js file into a single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
-});
-
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
+});
+
+// gulp.task('jsBrowserify', function() {
+//   return browserify({ entries: ['./js/pingpong-interface.js'] })
+//     .bundle()
+//     .pipe(source('app.js'))
+//     .pipe(gulp.dest('./build/js'));
+// });
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
